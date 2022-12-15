391fcca0bd086e3c0600c0a531840f922193095c
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-12-15 19:53:05 +0100

    check point 2 ( seting up local storage )

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 // query selector
-const listCont = document.querySelector('.all-tasks')
+const listCont = document.querySelector('[data-lists]')
+const listNameInput = document.querySelector('.new.list')
+const form = document.querySelector('[data-new-list-form]')
+const inputForm = document.querySelector('[data-new-list-input]')
+const deleteListBtn = document.querySelector('.delete-list')
+
+const lists = [
+];
+
+form.addEventListener('submit' , addList)
 
 
-const lists = [];
+function render(){
+
+    clearElement(listCont)
+
+
+
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add('list-name');
+        listElement.innerText= list.name;
+        listCont.appendChild(listElement)
+    })
+}
+
+function addList(e){
+
+
+    e.preventDefault();
+
+    if(inputForm.value === null || inputForm.value === '') alert('type something')
+    var newList = {};  
+    newList['id'] = Date.now().toString();
+    newList['name'] = inputForm.value,
+    newList['tasks'] = []
+    lists.push(newList);
+    inputForm.value = ''
+    render();
+}
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
