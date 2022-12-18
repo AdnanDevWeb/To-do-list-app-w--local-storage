9068163ec6e0b77d587b78ddb23d3c3032ed2efd
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-12-18 22:23:38 +0100

    one step before adding tasks

--- script.js
+++ script.js
@@ -5,33 +5,99 @@ const form = document.querySelector('[data-new-list-form]')
 const inputForm = document.querySelector('[data-new-list-input]')
 const deleteListBtn = document.querySelector('.delete-list')
 const listTitle = document.querySelector('.list-title')
-const newTaskForm = document.querySelector('.new.task')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const newTaskInput = document.querySelector('[data-new-task-input]')
+
+const todoCont = document.querySelector('.todo-body')
+
+// list
+
 const LOCAL_STORAGE_LIST_ID_KEY = "task.selectedListId"
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY)
 
+//task
+const LOCAL_STORAGE_TASK_ID_KEY = "task.selectedTaskId"
+const LOCAL_STORAGE_TASK_KEY = 'task.taks'
+const tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
+let selectedTaskId = localStorage.getItem(LOCAL_STORAGE_TASK_ID_KEY)
 
+
+
+newTaskForm.addEventListener('submit',addTask)
 form.addEventListener('submit' , addList)
+
+
 listCont.addEventListener('click', e =>{
     if(e.target.tagName === "LI"){
-        console.log(e.target.dataset.listId);
+        // console.log(e.target.dataset.listId);
         selectedListId = e.target.dataset.listId
         saveAndRender()
     } 
 })
 
 
+function addTask(e){
+    e.preventDefault()
+    if(newTaskInput.value === null || newTaskInput.value === '') return
+    console.log(selectedListId);
 
-function setTitle (){
-    const selectedList = document.querySelector(`[data-list-id="${selectedListId}"]`)
-    listTitle.textContent = selectedList.textContent
+    var newTask = {};
+    newTask['id'] = Date.now().toString();
+    newTask['name'] = newTaskInput.value;
+    newTask['done'] = false;
+    let selectedList =  lists.find(list => list.id === selectedListId);
+    console.log(selectedList);
+    selectedList.tasks.push(newTask)
+
+    newTaskInput.value = ""
+    saveAndRender()
 }
+
+  
+function addList(e){
+
+
+    e.preventDefault();
+
+    if(inputForm.value === null || inputForm.value === '') return
+    var newList = {};  
+    newList['id'] = Date.now().toString();
+    newList['name'] = inputForm.value,
+    newList['tasks'] = []
+    lists.push(newList);
+    inputForm.value = ''
+    saveAndRender()
+}
+
+// function setTitle (){
+//     const selectedList = document.querySelector(`[data-list-id="${selectedListId}"]`)
+//     listTitle.textContent = selectedList.textContent
+// }
+
+
 function render(){
 
     clearElement(listCont)
 
 
+    lists.forEach(task => {
+        // ac = actual just to sperate the var to "task"
+        const acTask = document.createElement('div')
+        acTask.classList.add('task');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        acTask.appendChild(checkbox)
+
+        const label = document.createElement('label');
+
+        const span = document.createElement('span')
+        span.classList.add('custom-checkbox')
+        label.textContent = task.name;
+
+    })
 
     lists.forEach(list => {
 
@@ -49,25 +115,11 @@ function save(){
     localStorage.setItem(LOCAL_STORAGE_LIST_ID_KEY,selectedListId)
 
 }
-function addList(e){
-
-
-    e.preventDefault();
-
-    if(inputForm.value === null || inputForm.value === '') alert('type something')
-    var newList = {};  
-    newList['id'] = Date.now().toString();
-    newList['name'] = inputForm.value,
-    newList['tasks'] = []
-    lists.push(newList);
-    inputForm.value = ''
-    saveAndRender()
-}
 
 function saveAndRender(){
     save();
     render();
-    setTitle()
+    // setTitle()
 }
 function clearElement(element){
     while(element.firstChild){
