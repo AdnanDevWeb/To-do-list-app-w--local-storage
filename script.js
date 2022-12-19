fa90de338b081c5de2fc17f99d4682ea86f727dc
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-12-19 12:33:43 +0100

    deleted everything

--- script.js
+++ script.js
@@ -13,21 +13,19 @@ const todoCont = document.querySelector('.tasks')
 // list
 
 const LOCAL_STORAGE_LIST_ID_KEY = "task.selectedListId"
+let LOCAL_STORAGE_LIST_ID_KEY = 'task.selectedListId'
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY)
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY)
 
-//task
-const LOCAL_STORAGE_TASK_ID_KEY = "task.selectedTaskId"
-const LOCAL_STORAGE_TASK_KEY = 'task.taks'
-const tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
-let selectedTaskId = localStorage.getItem(LOCAL_STORAGE_TASK_ID_KEY)
 
-
-
-newTaskForm.addEventListener('submit',addTask)
 form.addEventListener('submit' , addList)
 
+listCont.addEventListener('click' , e =>{
+    let target = e.target.dataset.listId;
+    localStorage.setItem(LOCAL_STORAGE_LIST_ID_KEY , target)
+})
 
 listCont.addEventListener('click', e =>{
     if(e.target.tagName === "LI"){
@@ -84,19 +82,6 @@ function render(){
 
     clearElement(listCont)
 
-
-    lists.forEach(list => {
-        // ac = actual just to sperate the var to "task"
-        list.tasks.forEach(task =>{
-            console.log(task.name)
-            const test = document.createElement('p')
-            test.textContent = task.name
-            todoCont.appendChild(test)            
-        })
-        
-
-    })
-
     lists.forEach(list => {
 
         const listElement = document.createElement('li')
@@ -106,18 +91,37 @@ function render(){
         if(list.id === selectedListId) listElement.classList.add('active-list')
 
         listCont.appendChild(listElement)
+
+        if(list.id === selectedListId){
+            list.classList.add('active-list')
+        }
     })
 }
 function save(){
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY , JSON.stringify(lists))
     localStorage.setItem(LOCAL_STORAGE_LIST_ID_KEY,selectedListId)
 
-}
 
 function saveAndRender(){
     save();
-    render();
-    // setTitle()
+    render()
+}
+
+function save(){
+    localStorage.setItem(  LOCAL_STORAGE_LIST_KEY , JSON.stringify(lists))
+}
+
+
+    e.preventDefault();
+
+    if(inputForm.value === null || inputForm.value === '') alert('type something')
+    var newList = {};  
+    newList['id'] = Date.now().toString();
+    newList['name'] = inputForm.value,
+    newList['tasks'] = []
+    lists.push(newList);
+    inputForm.value = ''
+    saveAndRender()
 }
 function clearElement(element){
     while(element.firstChild){
