a257b99db00041e87e7754e259d62a4f3076863d
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-12-19 16:35:49 +0100

    rn

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 // query selector
 const listCont = document.querySelector('[data-lists]')
-const listNameInput = document.querySelector('.new.list')
+const listNameInput = document.querySelector('.new-list')
 const form = document.querySelector('[data-new-list-form]')
 const inputForm = document.querySelector('[data-new-list-input]')
 const deleteListBtn = document.querySelector('.delete-list')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitle = listDisplayContainer.querySelector('[data-list-title]')
+const listCount = listDisplayContainer.querySelector('[data-list-count]')
+const tasks = listDisplayContainer.querySelector('[data-tasks]')
 
 let LOCAL_STORAGE_LIST_ID_KEY = 'task.selectedListId'
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY)
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 
 form.addEventListener('submit' , addList)
 
 listCont.addEventListener('click' , e =>{
-    let target = e.target.dataset.listId;
-    localStorage.setItem(LOCAL_STORAGE_LIST_ID_KEY , target)
+    if(e.target.tagName.toLowerCase()==='li'){
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
 })
+
+deleteListBtn.addEventListener('click' , deleteList)
+
+function deleteList(e){
+    const currentList = listCont.querySelector(`[data-list-id="${selectedListId}"]`)
+    const updatedArray = lists.filter(list => list.id !== selectedListId);
+    lists = updatedArray;
+    selectedListId = null
+    saveAndRender()
+    console.log(currentList);
+}
 
 function render(){
 
@@ -29,11 +46,12 @@ function render(){
         listElement.classList.add('list-name');
         listElement.innerText= list.name;
         listCont.appendChild(listElement)
-
         if(list.id === selectedListId){
-            list.classList.add('active-list')
+            listElement.classList.add('active-list')
         }
+        
     })
+    
 }
 
 function saveAndRender(){
@@ -43,21 +61,24 @@ function saveAndRender(){
 
 function save(){
     localStorage.setItem(  LOCAL_STORAGE_LIST_KEY , JSON.stringify(lists))
+    localStorage.setItem(  LOCAL_STORAGE_LIST_ID_KEY , selectedListId)
+
 }
 function addList(e){
 
 
     e.preventDefault();
 
-    if(inputForm.value === null || inputForm.value === '') alert('type something')
+    if(inputForm.value === null || inputForm.value === '' || inputForm.value === ' ') return alert('type something')
     var newList = {};  
     newList['id'] = Date.now().toString();
-    newList['name'] = inputForm.value,
-    newList['tasks'] = []
+    newList['name'] = inputForm.value;
+    newList['tasks'] = [];
     lists.push(newList);
     inputForm.value = ''
     saveAndRender()
 }
+
 function clearElement(element){
     while(element.firstChild){
         element.removeChild(element.firstChild)
